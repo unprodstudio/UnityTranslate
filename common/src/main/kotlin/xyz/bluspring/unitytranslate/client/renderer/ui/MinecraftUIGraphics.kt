16eaa8ccf53bcc2e41d8158490ce390081dd178c
BluSpring <bluspringindohq@gmail.com>
2026-09-20 15:48:28 +0800

    disable the debug outlines

--- common/src/main/kotlin/xyz/bluspring/unitytranslate/client/renderer/ui/MinecraftUIGraphics.kt
+++ common/src/main/kotlin/xyz/bluspring/unitytranslate/client/renderer/ui/MinecraftUIGraphics.kt
@@ -14,7 +14,6 @@ import xyz.bluspring.unitytranslate.api.v2.client.gui.TextureReference
 import xyz.bluspring.unitytranslate.api.v2.client.gui.UIGraphics
 import xyz.bluspring.unitytranslate.api.v2.client.gui.font.FontReference
 import xyz.bluspring.unitytranslate.api.v2.display.text.TextComponent
-import xyz.bluspring.unitytranslate.api.v2.util.ARGBHelper
 import xyz.bluspring.unitytranslate.client.ClientPlatformProxy
 import xyz.bluspring.unitytranslate.client.renderer.ui.font.FreeTypeFontReference
 import xyz.bluspring.unitytranslate.client.renderer.ui.font.MinecraftFontReference
@@ -57,7 +56,7 @@ class MinecraftUIGraphics(private val graphics: GuiGraphicsExtractor) : UIGraphi
 
     override fun enableScissor(x: Int, y: Int, width: Int, height: Int) {
         awtRenderer.flushLayer(this)
-        this.outline(x.toFloat(), y.toFloat(), x + width.toFloat(), y + height.toFloat(), 2f, ARGBHelper.color(255, 255, 0, 0))
+//        this.outline(x.toFloat(), y.toFloat(), x + width.toFloat(), y + height.toFloat(), 2f, ARGBHelper.color(255, 255, 0, 0))
         graphics.enableScissor(x, y, x + width, y + height)
         awtRenderer.pushLayer(x, y, width, height, Matrix3x2f(this.graphics.pose()))
     }
